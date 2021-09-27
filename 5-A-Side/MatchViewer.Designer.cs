c3166d4d85d430e091ed598997fa8ea07505c2cd
Thomas DORRINGTON <016518@brgs.org.uk>
2021-09-27 11:30:03 +0100

    Add project files.

---
+++ 5-A-Side/MatchViewer.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace _5_A_Side
+{
+    partial class MatchViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MatchViewer));
+            this.label1 = new System.Windows.Forms.Label();
+            this.team1Name = new System.Windows.Forms.Label();
+            this.leftHome = new System.Windows.Forms.Label();
+            this.leftAway = new System.Windows.Forms.Label();
+            this.cpuTeamLabel = new System.Windows.Forms.Label();
+            this.rightHome = new System.Windows.Forms.Label();
+            this.rightAway = new System.Windows.Forms.Label();
+            this.userScoreLabel = new System.Windows.Forms.Label();
+            this.cpuScoreLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.matchTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(472, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(587, 106);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Match Viewer";
+            // 
+            // team1Name
+            // 
+            this.team1Name.AutoSize = true;
+            this.team1Name.BackColor = System.Drawing.Color.Transparent;
+            this.team1Name.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.team1Name.ForeColor = System.Drawing.Color.White;
+            this.team1Name.Location = new System.Drawing.Point(550, 151);
+            this.team1Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.team1Name.Name = "team1Name";
+            this.team1Name.Size = new System.Drawing.Size(186, 45);
+            this.team1Name.TabIndex = 1;
+            this.team1Name.Text = "User Team";
+            // 
+            // leftHome
+            // 
+            this.leftHome.AutoSize = true;
+            this.leftHome.BackColor = System.Drawing.Color.Transparent;
+            this.leftHome.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.leftHome.ForeColor = System.Drawing.Color.White;
+            this.leftHome.Location = new System.Drawing.Point(496, 151);
+            this.leftHome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.leftHome.Name = "leftHome";
+            this.leftHome.Size = new System.Drawing.Size(72, 45);
+            this.leftHome.TabIndex = 2;
+            this.leftHome.Text = "(H)";
+            // 
+            // leftAway
+            // 
+            this.leftAway.AutoSize = true;
+            this.leftAway.BackColor = System.Drawing.Color.Transparent;
+            this.leftAway.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.leftAway.ForeColor = System.Drawing.Color.White;
+            this.leftAway.Location = new System.Drawing.Point(496, 151);
+            this.leftAway.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.leftAway.Name = "leftAway";
+            this.leftAway.Size = new System.Drawing.Size(70, 45);
+            this.leftAway.TabIndex = 3;
+            this.leftAway.Text = "(A)";
+            this.leftAway.Visible = false;
+            // 
+            // cpuTeamLabel
+            // 
+            this.cpuTeamLabel.AutoSize = true;
+            this.cpuTeamLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cpuTeamLabel.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cpuTeamLabel.ForeColor = System.Drawing.Color.White;
+            this.cpuTeamLabel.Location = new System.Drawing.Point(730, 151);
+            this.cpuTeamLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cpuTeamLabel.Name = "cpuTeamLabel";
+            this.cpuTeamLabel.Size = new System.Drawing.Size(180, 45);
+            this.cpuTeamLabel.TabIndex = 4;
+            this.cpuTeamLabel.Text = "CPU Team";
+            // 
+            // rightHome
+            // 
+            this.rightHome.AutoSize = true;
+            this.rightHome.BackColor = System.Drawing.Color.Transparent;
+            this.rightHome.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rightHome.ForeColor = System.Drawing.Color.White;
+            this.rightHome.Location = new System.Drawing.Point(888, 151);
+            this.rightHome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.rightHome.Name = "rightHome";
+            this.rightHome.Size = new System.Drawing.Size(72, 45);
+            this.rightHome.TabIndex = 5;
+            this.rightHome.Text = "(H)";
+            this.rightHome.Visible = false;
+            // 
+            // rightAway
+            // 
+            this.rightAway.AutoSize = true;
+            this.rightAway.BackColor = System.Drawing.Color.Transparent;
+            this.rightAway.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rightAway.ForeColor = System.Drawing.Color.White;
+            this.rightAway.Location = new System.Drawing.Point(890, 151);
+            this.rightAway.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.rightAway.Name = "rightAway";
+            this.rightAway.Size = new System.Drawing.Size(70, 45);
+            this.rightAway.TabIndex = 6;
+            this.rightAway.Text = "(A)";
+            // 
+            // userScoreLabel
+            // 
+            this.userScoreLabel.AutoSize = true;
+            this.userScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.userScoreLabel.Font = new System.Drawing.Font("Segoe UI Black", 120F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.userScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.userScoreLabel.Location = new System.Drawing.Point(-31, 305);
+            this.userScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.userScoreLabel.Name = "userScoreLabel";
+            this.userScoreLabel.Size = new System.Drawing.Size(370, 425);
+            this.userScoreLabel.TabIndex = 7;
+            this.userScoreLabel.Text = "0";
+            // 
+            // cpuScoreLabel
+            // 
+            this.cpuScoreLabel.AutoSize = true;
+            this.cpuScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cpuScoreLabel.Font = new System.Drawing.Font("Segoe UI Black", 120F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cpuScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.cpuScoreLabel.Location = new System.Drawing.Point(301, 305);
+            this.cpuScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cpuScoreLabel.Name = "cpuScoreLabel";
+            this.cpuScoreLabel.Size = new System.Drawing.Size(370, 425);
+            this.cpuScoreLabel.TabIndex = 8;
+            this.cpuScoreLabel.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(243, 435);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 212);
+            this.label2.TabIndex = 9;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(635, 382);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(435, 318);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Match\r\nClock:";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TimerLabel.Font = new System.Drawing.Font("Segoe UI Black", 120F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TimerLabel.ForeColor = System.Drawing.Color.White;
+            this.TimerLabel.Location = new System.Drawing.Point(988, 305);
+            this.TimerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(480, 425);
+            this.TimerLabel.TabIndex = 11;
+            this.TimerLabel.Text = "0\'";
+            // 
+            // matchTimer
+            // 
+            this.matchTimer.Interval = 7000;
+            this.matchTimer.Tick += new System.EventHandler(this.matchTimer_Tick);
+            // 
+            // MatchViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1456, 877);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cpuScoreLabel);
+            this.Controls.Add(this.team1Name);
+            this.Controls.Add(this.cpuTeamLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userScoreLabel);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.leftHome);
+            this.Controls.Add(this.rightAway);
+            this.Controls.Add(this.rightHome);
+            this.Controls.Add(this.leftAway);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "MatchViewer";
+            this.Text = "Match Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label team1Name;
+        private System.Windows.Forms.Label leftHome;
+        private System.Windows.Forms.Label leftAway;
+        private System.Windows.Forms.Label cpuTeamLabel;
+        private System.Windows.Forms.Label rightHome;
+        private System.Windows.Forms.Label rightAway;
+        private System.Windows.Forms.Label userScoreLabel;
+        private System.Windows.Forms.Label cpuScoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Timer matchTimer;
+    }
+}
---
+++ 5-A-Side/MatchViewer.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _5_A_Side
+{
+    public partial class MatchViewer : Form
+    {
+        //variables for individual player attributes (user team);
+        
+        //captain attribute variables
+        public int p1ShootingVal = 80;
+        public int p1DribblingVal = 80;
+        public int p1PaceVal = 80;
+        public int p1PhysicalVal = 80;
+        public int p1ReliableVal = 80;
+        public int p1TackleVal = 80;
+        public int p1AggroVal = 80;
+        //player 2 attribute variables
+        public int p2ShootingVal = 80;
+        public int p2DribblingVal = 80;
+        public int p2PaceVal = 80;
+        public int p2PhysicalVal = 80;
+        public int p2ReliableVal = 80;
+        public int p2TackleVal = 80;
+        public int p2AggroVal = 80;
+        //player 3 attribute variables;
+        public int p3ShootingVal = 80;
+        public int p3DribblingVal = 80;
+        public int p3PaceVal = 80;
+        public int p3PhysicalVal = 80;
+        public int p3ReliableVal = 80;
+        public int p3TackleVal = 80;
+        public int p3AggroVal = 80;
+        //player 4 attribute variables;
+        public int p4ShootingVal = 80;
+        public int p4DribblingVal = 80;
+        public int p4PaceVal = 80;
+        public int p4PhysicalVal = 80;
+        public int p4ReliableVal = 80;
+        public int p4TackleVal = 80;
+        public int p4AggroVal = 80;
+        //player 5 attribute variables;
+        public int p5ShootingVal = 80;
+        public int p5DribblingVal = 80;
+        public int p5PaceVal = 80;
+        public int p5PhysicalVal = 80;
+        public int p5ReliableVal = 80;
+        public int p5TackleVal = 80;
+        public int p5AggroVal = 80;
+        //substitute attribute variables
+        public int subShootingVal = 80;
+        public int subDribblingVal = 80;
+        public int subPaceVal = 80;
+        public int subPhysicalVal = 80;
+        public int subReliableVal = 80;
+        public int subTackleVal = 80;
+        public int subAggroVal = 80;
+
+        //variables for individual player attributes (user team);
+
+        //captain attribute variables
+        public int cpuP1ShootingVal = 80;
+        public int cpuP1DribblingVal = 80;
+        public int cpuP1PaceVal = 80;
+        public int cpuP1PhysicalVal = 80;
+        public int cpuP1ReliableVal = 80;
+        public int cpuP1TackleVal = 80;
+        public int cpuP1AggroVal = 80;
+        //player 2 attribute variables
+        public int cpuP2ShootingVal = 80;
+        public int cpuP2DribblingVal = 80;
+        public int cpuP2PaceVal = 80;
+        public int cpuP2PhysicalVal = 80;
+        public int cpuP2ReliableVal = 80;
+        public int cpuP2TackleVal = 80;
+        public int cpuP2AggroVal = 80;
+        //player 3 attribute variables;
+        public int cpuP3ShootingVal = 80;
+        public int cpuP3DribblingVal = 80;
+        public int cpuP3PaceVal = 80;
+        public int cpuP3PhysicalVal = 80;
+        public int cpuP3ReliableVal = 80;
+        public int cpuP3TackleVal = 80;
+        public int cpuP3AggroVal = 80;
+        //player 4 attribute variables;
+        public int cpuP4ShootingVal = 80;
+        public int cpuP4DribblingVal = 80;
+        public int cpuP4PaceVal = 80;
+        public int cpuP4PhysicalVal = 80;
+        public int cpuP4ReliableVal = 80;
+        public int cpuP4TackleVal = 80;
+        public int cpuP4AggroVal = 80;
+        //player 5 attRibute variables;
+        public int cpuP5ShootingVal = 80;
+        public int cpuP5DribblingVal = 80;
+        public int cpuP5PaceVal = 80;
+        public int cpuP5PhysicalVal = 80;
+        public int cpuP5ReliableVal = 80;
+        public int cpuP5TackleVal = 80;
+        public int cpuP5AggroVal = 80;
+        //substitute attribute variables
+        public int cpuSubShootingVal = 80;
+        public int cpuSubDribblingVal = 80;
+        public int cpuSubPaceVal = 80;
+        public int cpuSubPhysicalVal = 80;
+        public int cpuSubReliableVal = 80;
+        public int cpuSubTackleVal = 80;
+        public int cpuSubAggroVal = 80;
+
+        //variables for averages of each attribute for each stat for user and cpu teams
+        public int userSHO;
+        public int userDRI;
+        public int userPAC;
+        public int userPHY;
+        public int userREL;
+        public int userTAC;
+        public int userAGG;
+        public int cpuSHO;
+        public int cpuDRI;
+        public int cpuPAC;
+        public int cpuPHY;
+        public int cpuREL;
+        public int cpuTAC;
+        public int cpuAGG;
+        public int userScoreChance;
+        public int cpuScoreChance;
+        public bool matchEnded = false;
+        public int gameClockIncrementer = 0;
+        public bool userAtHome = true;
+
+        public MatchViewer()
+        {
+            InitializeComponent();
+            LoadTeams();
+            matchTimer.Enabled = true;
+        }
+
+        public void LoadTeams()
+        {
+            //need to add the loading of team names to the labels and proper iteration through the teams from CPU
+            //Still need to pull who is home and who is away from fixture table to then control this variable
+            if(userAtHome == false)
+            {
+                leftHome.Hide();
+                leftAway.Show();
+                rightAway.Hide();
+                rightHome.Show();
+            }
+            TeamAvgStats();
+            userScoreChance = UserChanceGenerator();
+            cpuScoreChance = CPUChanceGenerator();
+        }
+
+        public void TeamAvgStats()
+        {
+            userSHO = (p1ShootingVal + p2ShootingVal + p3ShootingVal + p4ShootingVal + p5ShootingVal) / 5;
+            userDRI = (p1DribblingVal + p2DribblingVal + p3DribblingVal + p4DribblingVal + p5DribblingVal) / 5;
+            userPAC = (p1PaceVal + p2PaceVal + p3PaceVal + p4PaceVal + p5PaceVal) / 5;
+            userPHY = (p1PhysicalVal + p2PhysicalVal + p3PhysicalVal + p4PhysicalVal + p5PhysicalVal) / 5;
+            userREL = (p1ReliableVal + p2ReliableVal + p3ReliableVal + p4ReliableVal + p5ReliableVal) / 5;
+            userTAC = (p1TackleVal + p2TackleVal + p3TackleVal + p4TackleVal + p5TackleVal) / 5;
+            userAGG = (p1AggroVal + p2AggroVal + p3AggroVal + p4AggroVal + p5AggroVal) / 5;
+            cpuSHO = (cpuP1ShootingVal + cpuP2ShootingVal + cpuP3ShootingVal + cpuP4ShootingVal + cpuP5ShootingVal) / 5;
+            cpuDRI = (cpuP1DribblingVal + cpuP2DribblingVal + cpuP3DribblingVal + cpuP4DribblingVal + cpuP5DribblingVal) / 5;
+            cpuPAC = (cpuP1PaceVal + cpuP2PaceVal + cpuP3PaceVal + cpuP4PaceVal + cpuP5PaceVal) / 5;
+            cpuPHY = (cpuP1PhysicalVal + cpuP2PhysicalVal + cpuP3PhysicalVal + cpuP4PhysicalVal + cpuP5PhysicalVal) / 5;
+            cpuREL = (cpuP1ReliableVal + cpuP2ReliableVal + cpuP3ReliableVal + cpuP4ReliableVal + cpuP5ReliableVal) / 5;
+            cpuTAC = (cpuP1TackleVal + cpuP2TackleVal + cpuP3TackleVal + cpuP4TackleVal + cpuP5TackleVal) / 5;
+            cpuAGG = (cpuP1AggroVal + cpuP2AggroVal + cpuP3AggroVal + cpuP4AggroVal + cpuP5AggroVal) / 5;
+        }
+
+        public int UserChanceGenerator()
+        {
+            userScoreChance = Convert.ToInt32(((0.6 * userSHO) + (0.5 * userPAC) + (0.3 * userDRI) + (0.1 * userPHY)) / 4); //loading the user attacking attribute stats into weighted average equation
+            userScoreChance = userScoreChance - Convert.ToInt32(((0.5 * cpuTAC) + (0.4 * cpuPHY) + (0.2 * cpuAGG) + (0.2 * cpuREL)) / 4); //weighted average then found using CPU defensive attribute stats
+            if (userAtHome == true)
+            {
+                userScoreChance = Convert.ToInt32(Convert.ToDouble(userScoreChance) / 0.42857142857); //home adv. modifier = 3:7 ratio of A:H fans so we 3/7 as modifier
+            }
+            
+            else
+            {
+                userScoreChance = Convert.ToInt32(Convert.ToDouble(userScoreChance) / 2.33333333333); //away disadv. modifier = 7:3 ratio of H:A fans so we use 7/3 as modifier
+            }
+            return userScoreChance;
+        }
+
+        public int CPUChanceGenerator()
+        {
+            cpuScoreChance = Convert.ToInt32(((0.6 * cpuSHO) + (0.5 * cpuPAC) + (0.3 * cpuDRI) + (0.1 * cpuPHY)) / 4); //loading the CPU attacking attribute stats into weighted average equation
+            cpuScoreChance = cpuScoreChance - Convert.ToInt32(((0.5 * userTAC) + (0.4 * userPHY) + (0.2 * userAGG) + (0.2 * userREL)) / 4); //weighted average then found using user defensive attribute stats
+            if (userAtHome == false)
+            {
+                cpuScoreChance = Convert.ToInt32(Convert.ToDouble(cpuScoreChance) / 0.42857142857); //home adv. modifier = 3:7 ratio of A:H fans so we 3/7 as modifier
+            }
+
+            else
+            {
+                cpuScoreChance = Convert.ToInt32(Convert.ToDouble(cpuScoreChance) / 2.33333333333); //away disadv. modifier = 7:3 ratio of H:A fans so we use 7/3 as modifier
+            }
+            return cpuScoreChance;
+        }
+
+        public void ScoreGoalCheck()
+        {
+            Random random = new Random();
+            int rollUserProbabilities = random.Next(0, 100);
+            int rollCPUProbabilities = random.Next(0, 100);
+            if (rollUserProbabilities <= userScoreChance)
+            {
+                UserScoreGoal();   
+            }
+            if (rollCPUProbabilities <= cpuScoreChance)
+            {
+                CPUScoreGoal();
+            }
+        }
+
+        public void UserScoreGoal()
+        {
+            userScoreLabel.Text = Convert.ToString(Convert.ToInt32(userScoreLabel.Text) + 1);
+            string userScoreMsg = "Your team has scored. The score is now " + userScoreLabel.Text + " - " + cpuScoreLabel.Text;
+            string userScoreMsgTitle = TimerLabel.Text + " GOAL!";
+            matchTimer.Stop();
+            MessageBox.Show(userScoreMsg, userScoreMsgTitle);
+            matchTimer.Start();
+        }
+
+        public void CPUScoreGoal()
+        {
+            cpuScoreLabel.Text = Convert.ToString(Convert.ToInt32(cpuScoreLabel.Text) + 1);
+            string cpuScoreMsg = "The opposition team has scored. The score is now " + userScoreLabel.Text + " - " + cpuScoreLabel.Text;
+            string cpuScoreMsgTitle = TimerLabel.Text + " GOAL!";
+            matchTimer.Stop();
+            MessageBox.Show(cpuScoreMsg, cpuScoreMsgTitle);
+            matchTimer.Start();
+        }
+
+        private void matchTimer_Tick(object sender, EventArgs e)
+        {
+           if (matchEnded == false)
+           {
+                TimerLabel.Text = Convert.ToString(gameClockIncrementer + 5) + "'";
+                gameClockIncrementer += 5;
+                ScoreGoalCheck();
+                if (gameClockIncrementer == 90)
+                {
+                    matchEnded = true;
+                    string matchEndMsg = "The Match has ended, the game finished " + userScoreLabel.Text + " - " + cpuScoreLabel.Text;
+                    string matchEndMsgTitle = "Match Ended";
+                    MessageBox.Show(matchEndMsg, matchEndMsgTitle);
+                }
+            }
+        }
+    }
+}
