0dea555c91eaa236065c5a996eef451fdc107375
Thomas DORRINGTON <016518@brgs.org.uk>
2021-09-29 15:43:13 +0100

    messing around with insert method

--- 5-A-Side/SignUpForm.cs
+++ 5-A-Side/SignUpForm.cs
@@ -24,10 +24,14 @@ namespace _5_A_Side
             using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.FiveAsideDBConnectionString))
             {
                 connection.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO dbo.[User] (Username) VALUES (@Username)", connection);
+                SqlCommand cmd = new SqlCommand("INSERT INTO dbo.[User] (Username, Password, Name) VALUES (@Username, @Password, @Name)", connection);
                 cmd.Connection = connection;
-                cmd.Parameters.Add("@Username", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@Username", SqlDbType.NVarChar, 50);
                 cmd.Parameters["@Username"].Value = userNameTxt.Text;
+                cmd.Parameters.Add("@Password", SqlDbType.NVarChar, 50);
+                cmd.Parameters["@Password"].Value = PWTxt.Text;
+                cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 50);
+                cmd.Parameters["@Name"].Value = textBox1.Text;
                 MessageBox.Show(Convert.ToString(cmd.ExecuteNonQuery()), "Siuuu");
             }
             //this.Close();
