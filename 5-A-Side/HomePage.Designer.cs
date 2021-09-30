27f02a90772421bdfd2e2bf6c79f14c3ff45adaa
Thomas DORRINGTON <016518@brgs.org.uk>
2021-09-30 14:08:44 +0100

    addded label and button to home page

--- 5-A-Side/HomePage.Designer.cs
+++ 5-A-Side/HomePage.Designer.cs
@@ -30,21 +30,51 @@ namespace _5_A_Side
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TitleLabel.Font = new System.Drawing.Font("Segoe UI Black", 30F, System.Drawing.FontStyle.Bold);
+            this.TitleLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.TitleLabel.Location = new System.Drawing.Point(43, 9);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(699, 54);
+            this.TitleLabel.TabIndex = 2;
+            this.TitleLabel.Text = "Five-A-Side Fantasy Dream League";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 73);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // HomePage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(774, 379);
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TitleLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "HomePage";
             this.Text = "HomePage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
